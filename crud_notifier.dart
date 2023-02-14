abb3328c769e78bf9dd995986de906508bb3eb6f
Jboy Hashimoto <73347485+sakurakotubaki@users.noreply.github.com>
2023-02-14 21:35:52 +0900

    add Notifierについて
    Riverpod2.0で推奨されている書き方

---
+++ crud_notifier.dart
@@ -0,0 +1,46 @@
+Riverpod2.0で推奨されている複雑な操作をするときに使用するNotifierを使用して、Firestoreを操作する.
+  
+  ```dart
+  import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:counter/model/item_class.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Notifierクラスを使うためのProvider.
+final crudProvider = NotifierProvider<Crud, List<Item>>(Crud.new);
+// Firestoreを操作するNotifierクラス
+class Crud extends Notifier<List<Item>> {
+  @override
+  List<Item> build() {
+    return [];
+  }
+
+  // Firestoreのコレクションにアクセスする変数.
+  CollectionReference get collection =>
+      FirebaseFirestore.instance.collection('item');
+
+  // Firestoreにデータを追加するメソッド.
+  Future<DocumentReference> addItem(String title, String description) async {
+    final now = DateTime.now();
+    return await collection.add({
+      'title': title,
+      'description': description,
+      'createdAt': Timestamp.fromDate(now),
+    });
+  }
+
+  // Firestoreのデータを更新するメソッド.
+  Future<void> updateItem(String id, String title, String description) async {
+    final now = DateTime.now();
+    await collection.doc(id).update({
+      'title': title,
+      'description': description,
+      'updatedAt': Timestamp.fromDate(now),
+    });
+  }
+
+  // Firestoreのデータを削除するメソッド.
+  Future<void> deleteItem(String id) async {
+    await collection.doc(id).delete();
+  }
+}
+  ```
